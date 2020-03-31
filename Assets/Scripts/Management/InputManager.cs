2d596b3ed84ef091425649cb2b1ca44c1a83b97a
tinzie <benjamin.mitchell@live.co.uk>
2020-03-31 15:52:03 +0100

    Added Camera Zoom

--- Assets/Scripts/Management/InputManager.cs
+++ Assets/Scripts/Management/InputManager.cs
@@ -14,22 +14,31 @@ public class InputManager : MonoBehaviour
     private Vector2 previousPos;
     private Vector3 camLookAt;
     //
-    float distance;
+    float distance, previousDistance;
+    float minDistance = 5.0f;
+    float maxDistance = 25.0f;
     float rotationYAxis = 0.0f;
     float rotationXAxis = 0.0f;
     public float yMinLimit = -90f;
     public float yMaxLimit = 90f;
+    Quaternion rotation;
+    float previousTouchDistance;
 
     // Start is called before the first frame update
     void Start()
     {
         camLookAt = new Vector3(GameManager.Instance.mapX/2.0f,GameManager.Instance.mapY/2.0f,GameManager.Instance.mapZ/2.0f);
+
         distance = Vector3.Distance(cam.transform.position, camLookAt);
+        
         cam.transform.LookAt(camLookAt);
+
         Vector3 angles = cam.transform.eulerAngles;
         rotationYAxis = angles.y;
         rotationXAxis = angles.x;
 
+        Quaternion toRotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
+        rotation = toRotation;
     }
 
     // Update is called once per frame
@@ -38,11 +47,11 @@ public class InputManager : MonoBehaviour
 
         //object tap control
 #if (UNITY_EDITOR || UNITY_STANDALONE_WIN)
-        if (Input.GetMouseButtonUp(0)/*Input.GetMouseButtonDown(0)*/)
+        if (Input.GetMouseButtonDown(0)/*Input.GetMouseButtonUp(0)*/)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 #elif (UNITY_ANDROID || UNITY_IOS)
-        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Ended /*TouchPhase.Began*/))
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Ended /*TouchPhase.Began*/))   //might need to modify this for zooming -> rotating change
         {
                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
 #endif
@@ -55,20 +64,53 @@ public class InputManager : MonoBehaviour
                 }
             }
         }
+#if ((UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR)
+        else if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began /*TouchPhase.Began*/))
+        {/*if ((Input.touchCount > 1) && (!dragging))*/
+                dragging = true;
+                previousPos = getInputScreenPos(0);
+        }
+#endif
+
+        //camera zoom control
+#if (UNITY_EDITOR || UNITY_STANDALONE_WIN)
+        distance -= Input.GetAxis("Mouse ScrollWheel");
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+#elif (UNITY_ANDROID || UNITY_IOS)
+        if(Input.touchCount == 2)
+        {
+            Vector2 pos0 = getInputScreenPos(0);
+            Vector2 pos1 = getInputScreenPos(1);
+            
+            if(Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began)
+            {
+                previousTouchDistance = Mathf.Abs(Vector2.Distance(pos0, pos1));
+            }
+            else
+            {
+                float touchDist = Mathf.Abs(Vector2.Distance(pos0, pos1));
+                float distanceDifference = (touchDist - previousTouchDistance) * Time.deltaTime;
+                distance += distanceDifference;
+                distance = Mathf.Clamp(distance, minDistance, maxDistance);
+                previousTouchDistance = touchDist;
+            }
+            
+        }
+        else{
+            previousTouchDistance = 0.0f;
+        }
+
+#endif
 
         //camera rotation control
 #if (UNITY_EDITOR || UNITY_STANDALONE_WIN)
         //On desktop, rotate by holding the right click.
         if (Input.GetMouseButtonDown(1))
-#elif (UNITY_ANDROID || UNITY_IOS)
-        if ((Input.touchCount > 1) && (!dragging))
-#endif
         {
             dragging = true;
             previousPos = getInputScreenPos(0);
         }
-
-        
+#endif
 
         if(dragging)
         {
@@ -76,15 +118,13 @@ public class InputManager : MonoBehaviour
 #if (UNITY_EDITOR || UNITY_STANDALONE_WIN)
             if (Input.GetMouseButtonUp(1))
 #elif (UNITY_ANDROID || UNITY_IOS)
-            if ((Input.touchCount < 2) && (dragging))
+            if ((Input.touchCount != 1))    //should stop rotating if either 0 taps, or 2 taps (zoom)
 #endif
             {
                 dragging = false;
                 return;
             }
             
-            Debug.Log("Dragging");
-            Debug.Log(camLookAt);
             Vector2 currentPos = getInputScreenPos(0);
             Vector2 direction = currentPos - previousPos;
 
@@ -93,12 +133,16 @@ public class InputManager : MonoBehaviour
             rotationYAxis += direction.x;
             rotationXAxis -= direction.y;
             rotationXAxis = ClampAngle(rotationXAxis, yMinLimit, yMaxLimit);
-            Quaternion fromRotation = Quaternion.Euler(cam.transform.rotation.eulerAngles.x, cam.transform.rotation.eulerAngles.y, 0);
             Quaternion toRotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
-            Quaternion rotation = toRotation;
+            rotation = toRotation;
             cam.transform.rotation = rotation;
 
 
+            
+        }
+
+        if(dragging || distance != previousDistance)
+        {
             Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
             Vector3 position = rotation * negDistance + camLookAt;
             cam.transform.position = position;
@@ -115,14 +159,14 @@ public class InputManager : MonoBehaviour
          return Mathf.Clamp(angle, min, max);
     }
 
-
+    
     Vector2 getInputScreenPos(int n)
     {
         Vector3 pos;
 #if (UNITY_EDITOR || UNITY_STANDALONE_WIN)
         pos = Input.mousePosition;
 #elif (UNITY_ANDROID || UNITY_IOS)
-        pos = Input.GetTouch(0).position;
+        pos = Input.GetTouch(n).position;
 #endif
         Vector2 retPos = new Vector2(pos.x, pos.y);
         return retPos;

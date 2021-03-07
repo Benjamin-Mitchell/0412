b9f3d55a902d52ed0a240f419655a3f4343fb475
tinzie <benjamin.mitchell@live.co.uk>
2021-03-07 15:05:20 +0000

    Rotate script for Mat

---
+++ Assets/Scripts/SceneObjects/BaseRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseRotate : MonoBehaviour
+{
+	public Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Rotate(rotation);
+    }
+}

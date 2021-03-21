fa5353c6d4528656268419792e9068b7afa4b5c1
tinzie <benjamin.mitchell@live.co.uk>
2021-03-21 12:57:31 +0000

    New Render Distance Script for LoDs

---
+++ Assets/Scripts/Utility/RenderDistance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RenderDistance : MonoBehaviour
+{
+	private GameObject cam;
+
+	[Serializable]
+	public struct PSystem
+	{
+		[SerializeField]
+		public GameObject sys;
+
+		[SerializeField]
+		public float distanceToStopRendering;
+	}
+
+	public PSystem[] PSystems;
+    // Start is called before the first frame update
+    void Start()
+    {
+		cam = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		for(int i = 0; i < PSystems.Length; i++)
+		{
+			if(Vector3.Distance(cam.transform.position, transform.position) > PSystems[i].distanceToStopRendering)
+			{
+				PSystems[i].sys.SetActive(false);
+			}else
+			{
+				PSystems[i].sys.SetActive(true);
+			}
+		}
+    }
+}
